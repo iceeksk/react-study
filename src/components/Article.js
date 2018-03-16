f5821760562da4923b1e169bbb98e7febbec60ef
Kostiantyn.Sikhnevych <Kostiantyn.Sikhnevych@infopulse.com>
2018-03-16 11:37:14 +0200

    update(*): component with change state

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Article(props) {
-    console.dir(props);
+class Article extends Component{
 
-    const {article} = props;
+    state = {
+        isOpen: false
+    };
 
-    console.dir(article);
-    const body = <section>{article.text}</section>;
-    return (
-        <div>
-            <h2>title</h2>
-            {body}
-            <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
-        </div>
-    )
-};
+    render(){
+        const {article} = this.props;
+        const body = this.state.isOpen && <section>{article.text}</section>;
+        return (
+            <div>
+                <h2>
+                    {article.title}
+                    <button onClick={this.handleClick}>close</button>
+                </h2>
+                {body}
+                <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
+            </div>
+        )
+    }
+
+    handleClick = () => {
+        console.log("-------", "click");
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+}
+
 
 export default Article;
